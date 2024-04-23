2f20390ab6268eeb7e1bf24d6d42a24d89b30565
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-04-23 17:59:17 +0000

    save

--- js/splashScene.js
+++ js/splashScene.js
@@ -26,3 +26,33 @@ class SplashScene extends Phaser.Scene {
  init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
  }
+
+ /**
+  * Can be defined on your own Scenes.
+  * Yse it to ;oad assets
+ */
+ preload() {
+   console.log("Splash Scene")
+ }
+
+ /**
+  * Can be defined on your own Scenes.
+  * Use it to create yoyr objects
+  * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+  */
+ create(data) {
+   //pass
+ }
+
+ /**
+  * Should be overridden by your own Scenes.
+  * This method is called once per game step while the scene is running.
+  * @param {number} time - The current time.
+  * @param {number} delta - The delta time in ms since the last frame.
+  */
+    update(time, delta) {
+      // pass
+    }
+   }
+
+   export default SplashScene
