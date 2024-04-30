734b6a4c4764bec47d4d4061f4832df74c7e95ed
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-04-30 18:27:29 +0000

    save all changes/adding

--- js/game.js
+++ js/game.js
@@ -3,39 +3,46 @@
 // Copyright (c) 2024 Hames.al-sharoa ALL rights reserved
 //
 // Created by: Hames.al-sharoa
+// created on: apr 2024
 // This is the phaser3 game configuration file
-// This is the Phaser3 game configuration file
 
-// sene import statements
-import SplashScene from "./splashScene.js"
+ // scene import statements
+ import SplasgScene from "./splashScene.js"
+ import TitleScene from "./TitleScene.js"
 
-// create the new scenes
-  const splashScene = new SplashScene()
+ // create the new scene
+ const splashScene = new SplashScene()
+ const titleScene = new TitleScene()
 
-/** 
- * Start  Phaser Game.
-  */
+/**
+ * start phaser Game.
+ */
  const config = {
-   type: Phaser.AUTO,
-   width: 1920,
-   height: 1080,
-    physics: {
-      default: "arcade",
-      arcade: {
-        debug: true,
-      },
+  Type: Phaser.AUT0,
+  Width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
     },
+  },
   // set background color
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
-    autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.CENTER_BOTH
   },
 }
 
-const game = new Phaser.Game(config)
-console.log(game)
+ const game = new Phaser.Game(config)
+ // console.log(game)
 
-// the start scene
-game.scene.start("splashScene")
+ // load scenes
+ // Note: remember any "key" is global and CAN NOT be reused!
+ ame.scene.add("splashScene", splashScene)
+ game.scene.add("titleScene", titleScene)
+
+ // the state scene
+ game.scene.start("splashScene")
--- js/splashScene.js
+++ js/splashScene.js
@@ -3,56 +3,56 @@
 // Copyright (c) 2024 Hames.al-sharoa ALL rights reserved
 //
 // Created by: Hames.al-sharoa
-// This is the Splasg Scene.
+// created on: apr 2024
+ // This is the phaser Scene 
 
-/**
- * This class is the Splash Scene
- */
-class SplashScene extends Phaser.Scene {
+ /**
+  * this class is the splash Scene.
+  */
+class splashScene extends Phaser.Scene {
   /**
-   * This method is the construtor.
-   */
+    * this method is the construtor
+    */
+    constructor() {
+      super({ key: "splashScene"})
+    }
 
-  constructor() {
-   super({ key: "splashScene"})
-}
+    /**
+     * can be definded on your own Scenes.
+     * This method is called by the Scene Manager when the scene starts,
+     *   before preload() and create()
+     *@param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+     */
+    Infinity(data) {
+      this.cameras.main.setBackgroundColor("ffffff")
+    }
 
-/**
- * Can be defined on your own Scenes.
- * This methos is called by thr Scene Manager when the scene starts,
- *   before preload() and  create().
- * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
- */
- init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
- }
-
- /**
-  * Can be defined on your own Scenes.
-  * Yse it to ;oad assets
- */
- preload() {
-   console.log("Splash Scene")
- }
-
- /**
-  * Can be defined on your own Scenes.
-  * Use it to create yoyr objects
-  * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-  */
- create(data) {
-   //pass
- }
-
- /**
-  * Should be overridden by your own Scenes.
-  * This method is called once per game step while the scene is running.
-  * @param {number} time - The current time.
-  * @param {number} delta - The delta time in ms since the last frame.
-  */
-    update(time, delta) {
+    /**
+     * Can be defined on your own Scenes/
+     * Use it to load assets.
+     */
+    preload(){
+      console.log("Splash Scene")
+    }
+    
+    /**
+     * an be defined on your own Scenes.,
+     * Use it to create your game objects.
+     * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start().
+     */
+    create(data) {
       // pass
     }
-   }
 
-   export default SplashScene
+    /**
+     * should be overridden by your own Scenes.
+     * This method is called once per game step while the scene is running.
+     * @param {number} time - The current time.
+     * @param {number} delta - The delta time in ms since the last frame. 
+     */
+    update(time, delta) {
+      this.scene.switch("titleScene")
+      }
+    }
+
+    export default SplashScene 
